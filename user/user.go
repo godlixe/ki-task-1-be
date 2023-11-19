d0cde9a982dca978a8a147db0c82977c8bacf30c
godlixe <allzllx19@gmail.com>
2023-11-19 23:13:44 +0700

    fix: check struct field zero value before decrypting

--- user/user.go
+++ user/user.go
@@ -62,7 +62,7 @@ func (u *User) DecryptUserData(guard *guard.Guard, key guard.Key) error {
 
 	for i := 0; i < userV.NumField(); i++ {
 		field := userV.Type().Field(i).Name
-		if !slices.Contains(unencryptedFields, field) {
+		if !slices.Contains(unencryptedFields, field) && !userV.Field(i).IsZero() {
 			value := userV.Field(i).Interface().(string)
 
 			hexValue, err := hex.DecodeString(value)
